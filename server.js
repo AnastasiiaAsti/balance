e45328e7a73d76d7b23f3db44c6e00b2b29c4c0b
Asti <astistateofmind@gmail.com>
2022-06-27 09:14:33 -0700

    mount method override

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
+const methodOverride = require('method-override')
 
 require('dotenv').config()
 
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
 
 app.use(session({
   secret: process.env.SECRET,
