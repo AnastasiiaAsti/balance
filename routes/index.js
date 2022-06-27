eb47c067faa9ea83074fc335a36d27125eddae14
Asti <astistateofmind@gmail.com>
2022-06-27 08:54:21 -0700

    init commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/asanas',
+    failureRedirect: '/asanas'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res) {
+  req.logout(function (err) {
+    if (err) console.log(err)
+    res.redirect('/asanas')
+  })
+})
+
+
+module.exports = router;
