693f2d699bfe749bb24316276dc8dd15c7366054
Asti <astistateofmind@gmail.com>
2022-06-27 10:12:05 -0700

    add img to asana model

--- models/asana.js
+++ models/asana.js
@@ -3,6 +3,19 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const asanaSchema = new Schema({
+    photosURLs: [
+        {
+            type: String,
+            validate: {
+                validator: function (value) {
+                    const urlPattern = /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%#!\-/]))?/;
+                    const urlRegExp = new RegExp(urlPattern);
+                    return value.match(urlRegExp);
+            },
+                message: props => `${props.value} is not a valid URL`
+            }
+        }
+    ],
     name: {
         type: String,
         required: true
