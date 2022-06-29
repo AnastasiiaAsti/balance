3f1b9de3bc9fddb9196347cc5565c60a0ec4d735
Asti <astistateofmind@gmail.com>
2022-06-29 10:55:34 -0700

    style change

--- controllers/asanas.js
+++ controllers/asanas.js
@@ -11,7 +11,7 @@ module.exports = {
 };
 
 function update(req, res) {
-    Asana.update(req.body, req.params.id)
+    Asana.save(req.body, req.params.id)
     res.redirect(`/asanas/${req.params.id}`)
 }
 
